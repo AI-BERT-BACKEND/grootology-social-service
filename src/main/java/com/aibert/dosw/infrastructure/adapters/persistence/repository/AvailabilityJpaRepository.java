0ff70c5a9987e46d5955a3e2f49f5ce57b546898
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-05-23 04:36:29 -0500

    Corrección de error en swagger

--- src/main/java/com/aibert/dosw/infrastructure/adapters/persistence/repository/AvailabilityJpaRepository.java
+++ src/main/java/com/aibert/dosw/infrastructure/adapters/persistence/repository/AvailabilityJpaRepository.java
@@ -1,10 +1,12 @@
 package com.aibert.dosw.infrastructure.adapters.persistence.repository;
 
 import com.aibert.dosw.infrastructure.adapters.persistence.entity.AvailabilityConfigEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AvailabilityJpaRepository extends JpaRepository<AvailabilityConfigEntity, UUID> {
+    @EntityGraph(attributePaths = "authorizedFriends")
     Optional<AvailabilityConfigEntity> findByUserId(UUID userId);
 }
